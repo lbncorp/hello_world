0a3c308f4df239e768f40456743fe21eca22040b
lbncorp <ulamin97@gmail.com>
2016-05-04 20:57:30 -0700

    Added files via upload

---
+++ hello.java
@@ -0,0 +1,17 @@
+package com;
+
+/**
+ * Created by ulamin on 5/4/2016.
+ */
+public class hello {
+
+    public static void main(String[] args) {
+
+        int sum1 = 895; int sum2 =55; int total=0;
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(total);
+        total = sum1 + sum2;
+
+    }
+}
